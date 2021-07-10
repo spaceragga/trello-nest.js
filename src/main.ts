3a58b9e367fdc1dae103d7feeea395c7c71b2f79
Ivan <raggamix@mail.ru>
2021-07-10 16:05:04 +0300

    feat: add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Trello Service')
+    .setDescription("Let's try to create a competitor for Trello")
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(4000);
 }
 bootstrap();
